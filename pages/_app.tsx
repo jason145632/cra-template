045d17b15d44a45201134a6e6a08fa813fcf62e4
jason145632 <jasontse145632@gmail.com>
2022-03-10 11:18:58 +0800

    fixx: MyApp did not return anything

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,10 +3,11 @@ import { ChakraProvider } from '@chakra-ui/react';
 import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return;
-  <ChakraProvider>
-    <Component {...pageProps} />
-  </ChakraProvider>;
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
 
 export default MyApp;
